1b09daad3a10a26ec9fd24569c17f78b9f98f43b
zhouzhengyu <14790298903@163.com>
2018-12-13 15:12:56 +0800

    task7.html

---
+++ task7/task7.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    var list = document.getElementsByClassName('one');
+    var time;
+    var rgb;
+$('#start').on('click',function(){
+    clearInterval(time);
+    time = setInterval(function () {
+        for (var i = 0; i < list.length; i++) {
+            list[i].style.backgroundColor ='';
+        }
+        start();
+    }, 1000);
+})
+$('#end').on('click',function(){
+    clearInterval(time);
+        for (var i = 0; i < list.length; i++) {
+            list[i].style.backgroundColor ='';
+        }
+})
+    function start(){
+        var one = Math.floor(Math.random()*list.length);
+        var two = Math.floor(Math.random()*list.length);
+        var three = Math.floor(Math.random()*list.length);
+        if(one == two){
+            one = Math.floor(Math.random()*list.length);
+        }else if(two == three){
+            two = Math.floor(Math.random()*list.length);
+        }else if(one == three){
+            three = Math.floor(Math.random()*list.length);
+        }
+        list[one].style.backgroundColor = 'rgb'+ color();
+        list[two].style.backgroundColor = 'rgb'+ color();
+        list[three].style.backgroundColor = 'rgb'+ color();
+    }
+    
+     function color(){
+        var rgba='(255, 166, 0)'
+        colors();
+        if(rgb==rgba){
+         colors();
+         return rgb;
+        }else{
+            return rgb;
+        }
+        
+     }  
+     function colors(){
+         var r=Math.floor(Math.random()*255);
+         var g=Math.floor(Math.random()*255);
+         var b=Math.floor(Math.random()*255);
+         rgb='('+r+','+g+','+b+')';
+}
+})
